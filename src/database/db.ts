5ba60fa62ec07d8a837f678795be2b9b479d2845
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2024-01-20 12:43:35 -0800

    test comment

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const url: string = process.env.MONGO_URI as string;
 let connection: typeof mongoose;
+/* comment*/
 
 /**
  * Makes a connection to a MongoDB database. If a connection already exists, does nothing
