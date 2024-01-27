c69774a6c3736e3c19a4a60d35a273de5c76a896
Sriya-Adunur <sadunur@calpoly.edu>
2024-01-26 21:09:34 -0800

    Feat: Created user schema for fetched data

--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -1,9 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 
+type IUser = {
+  username: string;
+  password: string;
+  userType: string;
+  firstName: string;
+  lastName: string;
+  phoneNum: string;
+  email: string;
+};
+
 //! Example user schema. Not guaranteed to work
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  userType : { type: String, required: true},
+  firstName : { type: String, required: true},
+  lastName : { type: String, required: true},
+  phoneNum : { type: String, required: true},
+  email: { type: String, required: true, unique: true }
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
+export type {IUser};
+
+
