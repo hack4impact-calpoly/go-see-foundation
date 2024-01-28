0bb58a7d0ccb212a1e2c94e5e1d63187b1001a25
tung-aiden <aatung@calpoly.edu>
2024-01-28 11:42:47 -0800

    Created event schema

---
+++ src/database/event.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { Schema, model } from 'mongoose';
+
+type IEvent = {
+  picture: String // assumes URL
+  alt: String
+  description: String
+  date: Date
+  name: String
+}
+
+const eventSchema = new Schema<IEvent>({
+  picture: { type: String, required: true },
+  alt: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  name: { type: String, required: true },
+})
+
+const Event = mongoose.models['events'] || mongoose.model('events', eventSchema)
+
+export default Event
