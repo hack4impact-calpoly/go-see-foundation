d1265be15b1dbf045936ee921aa4738e638bfb2c
Sean Nguyen <smnguyen745@gmail.com>
2024-02-09 22:35:09 -0800

    feat: mostly done with styling

--- src/app/pages/authentication/login/page.tsx
+++ src/app/pages/authentication/login/page.tsx
@@ -3,6 +3,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import "./login.css";
 import logoImage from "../../../images/GO-See-HLogo.fw_.png";
 
@@ -10,6 +11,7 @@ export default function LoginPage() {
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
+    remember: false,
   });
 
   function handleLogin(event: React.FormEvent<HTMLFormElement>): void {
@@ -38,17 +40,16 @@ export default function LoginPage() {
 
   return (
     <div className="loginPage">
+      <h1 className="welcomeTitle">Welcome back to your account!</h1>
       <form className="loginForm" onSubmit={handleLogin}>
-        <Image
+        {/* <Image
           className="logoImg"
           src={logoImage}
           alt="A picture of the Go See Foundation's Logo"
           width="650" // about 1/5 of the original image width
           height="190" // about 1/5 of the original image height
-        ></Image>
-
-        <h1 className="loginTitle">LOGIN</h1>
-
+        ></Image> */}
+        <h2 className="formTitle">LOGIN</h2>
         {/* <label htmlFor="email" className={style.loginInputLabel}>
             Enter Email
           </label> */}
@@ -61,7 +62,6 @@ export default function LoginPage() {
           onChange={handleLoginChange}
           required
         />
-
         {/* <label htmlFor="password" className={style.loginInputLabel}>
             Enter Password
           </label> */}
@@ -74,13 +74,32 @@ export default function LoginPage() {
           onChange={handleLoginChange}
           required
         />
-
+        <div className="rememberMeCheckbox">
+          <label htmlFor="rememberMe">
+            <input
+              type="checkbox"
+              id="rememberMe"
+              name="rememberMe"
+              className="inline"
+            />{" "}
+            <p id="rememberMeText" className="inline">
+              Remember me?
+            </p>
+          </label>
+        </div>
         <button className="loginButton" type="submit">
           LOG IN
         </button>
 
+        {/*TODO: change href to proper forget page*/}
+        <Link href="/" className="forgotPasswordLink">
+          <h3 className="forgotPasswordText">Forgot Password</h3>
+        </Link>
+
+        <div className="break"></div>
+
         <div className="signUp">
-          <h2 className="signUpTitle">No Account?</h2>
+          <h2 className="signUpTitle">Not a Member yet?</h2>
           <button className="signUpButton" type="button" onClick={handleSignUp}>
             SIGN UP
           </button>
