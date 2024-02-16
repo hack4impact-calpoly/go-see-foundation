31bc312c9448d89e84a92d4a5d40323d55a70ad8
Ida Voong <idavoong@gmail.com>
2024-02-16 00:36:45 -0800

    feat: menu icon and screen sizing

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../images/GO-See-HLogo.fw_.png";
+import menu from "../images/menu.png";
 import styles from "./navbar.module.css";
 
 export default function Navbar() {
@@ -19,22 +20,29 @@ export default function Navbar() {
           />
         </div>
         <div className={styles.mainButtons}>
-          <Link href="">
-            <button className={`${styles.button} ${styles.donate}`}>
-              DONATE
+          <div className={styles.mainTop}>
+            <Link href="">
+              <button className={`${styles.button} ${styles.donate}`}>
+                DONATE
+              </button>
+            </Link>
+            <Link href="/pages/authentication/login">
+              <button className={`${styles.button} ${styles.login}`}>
+                LOG IN
+              </button>
+            </Link>
+          </div>
+          <div className={styles.mainBottom}>
+            <Link href="/pages/authentication/createAccount">
+              <button className={`${styles.button} ${styles.join}`}>
+                JOIN US
+              </button>
+            </Link>
+            <button className={`${styles.button} ${styles.menu}`}>
+              MENU
+              <Image src={menu} alt="menu icon" width="20" height="15" />
             </button>
-          </Link>
-          <Link href="/pages/authentication/login">
-            <button className={`${styles.button} ${styles.login}`}>
-              LOG IN
-            </button>
-          </Link>
-          <Link href="/pages/authentication/createAccount">
-            <button className={`${styles.button} ${styles.join}`}>
-              JOIN US
-            </button>
-          </Link>
-          <button className={`${styles.button} ${styles.menu}`}>MENU ≡</button>
+          </div>
         </div>
       </div>
       <div className={styles.subbar}>
