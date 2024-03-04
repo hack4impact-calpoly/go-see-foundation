cbbd4f88f00314618b93b688d877051977f7254b
Sriya-Adunur <sadunur@calpoly.edu>
2024-03-03 17:43:15 -0800

    feat: added footer component to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
 import CreateAccount from "./pages/authentication/createAccount/page";
 
 export default function Home() {
