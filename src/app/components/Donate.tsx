ddc67774fca66f4d062d5eb1a72f4cbd1abce533
Aiden Tung <aidentung@Aidens-MacBook-Pro-9.local>
2024-03-07 17:40:48 -0800

    fixed donate pages according to figma

--- src/app/components/Donate.tsx
+++ src/app/components/Donate.tsx
@@ -1,12 +1,16 @@
+"use client"
 import { useState } from "react";
 import "./Donate.css";
-import donateImage from "../images/donateImage.png";
+import donateImage from "../images/donateImage.png"
 import Image from "next/image";
 
 export default function Donate() {
   // holds the user selections
   const [time, setTime] = useState("Once");
   const [amount, setAmount] = useState("$25");
+
+  const desc =
+    "By donating to the GO See Foundation, you are not just offering financial support; you are providing dignity, independence, and community to those who navigate life's challenges without the gift of perfect sight. Together, let's illuminate paths and create a brighter future for the  vision loss community. Donate today and be a beacon of light in someone's darkness.";
 
   /* handles changes with the time buttons */
   const timeOnClick = (e: any) => {
@@ -47,7 +51,7 @@ export default function Donate() {
 
   /* handle changes with the change of the input */
   const amountInput = (e: any) => {
-    /* user has decided to entire stuff in remove the selected from buttons */
+    /* user has decided to enter stuff in remove the selected from buttons */
     document.querySelectorAll(".amountButton").forEach((b) => {
       b.classList.remove("selected");
     });
@@ -64,50 +68,62 @@ export default function Donate() {
   };
 
   return (
-    <div className="donateComponent">
-      <h1>Your Donation will help us GO See More</h1>
-      <form className="donateForm" onSubmit={handleSubmit}>
-        <div className="donateTime">
-          <button className="timeButton right selected" onClick={timeOnClick}>
-            Give Once
-          </button>
-          <button className="timeButton left" onClick={timeOnClick}>
-            Monthly
-          </button>
-        </div>
-        <div className="donateAmount">
-          <div className="amntButtonDiv">
-            <button className="amountButton selected" onClick={amountOnClick}>
-              $25
-            </button>
-            <button className="amountButton" onClick={amountOnClick}>
-              $50
-            </button>
-            <button className="amountButton" onClick={amountOnClick}>
-              $100
-            </button>
-            <button className="amountButton" onClick={amountOnClick}>
-              $250
-            </button>
+    <div>
+      <h1>Your Donation will help us GO See More!</h1>
+      <div className="donateComponent">
+        <Image
+          className="donateImage"
+          src={donateImage}
+          alt="A photo of GO Foundation members and a dog"
+        ></Image>{" "}
+        <div className="description">{desc}</div>
+        <form className="donateForm" onSubmit={handleSubmit}>
+          <div className="donateAmount">
+            <p>Donation Amount:</p>
+            <div className="amntButtonDiv">
+              <button className="amountButton selected" onClick={amountOnClick}>
+                $25
+              </button>
+              <button className="amountButton" onClick={amountOnClick}>
+                $50
+              </button>
+              <button className="amountButton" onClick={amountOnClick}>
+                $100
+              </button>
+            </div>
+            <div className="amntButtonDiv">
+              <button className="amountButton" onClick={amountOnClick}>
+                $250
+              </button>
+              <button className="amountButton" onClick={amountOnClick}>
+                $500
+              </button>
+              <input
+                type="number"
+                className="amountButton"
+                min={0}
+                onChange={amountInput}
+                placeholder="$"
+              ></input>
+            </div>
           </div>
-          <p>Input your own Donation amount below:</p>
-          <input
-            type="number"
-            className="customInput"
-            min="0"
-            onChange={amountInput}
-            placeholder="$$$$$"
-          ></input>
-        </div>
-        <button type="submit">DONATE</button>
-      </form>
-      <Image
-        className="donateImage"
-        src={donateImage}
-        alt="A photo of GO Foundation members and a dog"
-        /*width = "500"
-            height= "500"*/
-      ></Image>{" "}
+          <div className="donateFrequency">
+            <p>Donation Frequency:</p>
+            <select onChange={(e) => setTime(e.target.value)}>
+              <option value="Once">One-Time</option>
+              <option value="Monthly">Monthly</option>
+              <option value="Monthly">Annualy</option>
+            </select>
+          </div>
+          <div className="personalInformation">
+            <p>Personal Information:</p>
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="email" placeholder="Email Address" />
+          </div>
+          <button type="submit">DONATE</button>
+        </form>
+      </div>
     </div>
   );
 }
