d70c8083b9d2e2543a3707692911070936ce859d
snguyen745 <smnguyen745@gmail.com>
2024-04-08 22:13:42 -0700

    feat: added jsonwebtoken library, generated token, need to complete secret key

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,24 +1,34 @@
 import connectDB from "@database/db";
 import { NextRequest, NextResponse } from "next/server";
 import Users, { IUser } from "@database/userSchema";
-const bcrypt = require("bcrypt"); 
+import jwt from "jsonwebtoken";
+const bcrypt = require("bcrypt");
 
 export async function POST(req: NextRequest) {
-    await connectDB();
-    try{
-        const {email, password} = await req.json()
-        if (!email || !password) {
-            return NextResponse.json("Failed: Login Incomplete", { status: 400 });
-        }
-        const user = await Users.findOne({email: email}).orFail();
-        const passwordsMatch = bcrypt.compareSync(password, user.password);
-        if (!passwordsMatch){
-            return NextResponse.json("Failed: Login Failed", {status: 400})
-        }
-        console.log(email)
-        return NextResponse.json("Success: Login Complete");
+  await connectDB();
+  try {
+    const { email, password } = await req.json();
+    // TODO: add JWT_SECRET
+    // const jwtSecretKey = process.env.JWT_SECRET;
+    const jwtSecretKey = "2182312c81187ab82bbe053df6b7aa55";
+
+    if (!email || !password) {
+      return NextResponse.json("Failed: Login Incomplete", { status: 400 });
     }
-    catch (err) {
-        return NextResponse.json(`${err}`, { status: 400 });
+    const user = await Users.findOne({ email: email }).orFail();
+    // TODO: change passwords in Mongo to be encrypted versions
+    const passwordsMatch = true; //bcrypt.compareSync(password, user.password);
+    if (!passwordsMatch) {
+      return NextResponse.json("Failed: Login Failed", { status: 400 });
     }
+
+    // Login successful, create JSON web token
+    const data = { signInTime: Date.now(), user };
+    const token = jwt.sign(data, jwtSecretKey);
+    console.log(data);
+    console.log(token);
+    return NextResponse.json({ message: "Success: Login Complete", token });
+  } catch (err) {
+    return NextResponse.json(`${err}`, { status: 400 });
+  }
 }
