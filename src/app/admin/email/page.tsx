e6071e83cac8a8f04c46f8b29043002f2334d0b1
canbarci <canbarci@calpoly.edu>
2024-04-17 02:37:48 -0700

    feat: added basic email sending functionality

--- src/app/admin/email/page.tsx
+++ src/app/admin/email/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useRef } from "react";
 import styles from "./emailPage.module.css";
+import emailjs from 'emailjs-com';
 
 export default function AdminPageEmail() {
   const [group, setGroup] = useState("Test1");
@@ -9,6 +10,31 @@ export default function AdminPageEmail() {
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
   const [scrollDropValue, setScrollDrop] = useState("Inbox");
+
+  const toRef = useRef<HTMLInputElement>(null);
+  const subjectRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleSend = async (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+
+    try {
+        const emailParams = {
+            to_name: toRef.current?.value, 
+            subject: subjectRef.current?.value,
+            message: messageRef.current?.value,
+        };
+    
+        await emailjs.send('service_9qu0p52', 'template_99zsbe8', emailParams, "Zw5OFi0OEo2qOzWRb");
+        alert("Email sent successfully.");
+
+        setTo("");
+        setSubject("");
+        setMessage("");
+    } catch (error) {
+        console.error(error);
+    }
+}
 
   return (
     <div className={styles.emailArea}>
@@ -29,7 +55,7 @@ export default function AdminPageEmail() {
           <EmailPreview />
         </div>
       </div>
-      <form className={styles.emailForm}>
+      <form className={styles.emailForm} onSubmit={handleSend}>
         <label htmlFor="group">Select Group:</label>
         <select
           className={styles.groupDropdown}
@@ -50,22 +76,27 @@ export default function AdminPageEmail() {
           type="text"
           className={styles.to_input}
           value={to_value}
-          onChange={(e) => setTo(e.target.value)}
+          ref={toRef}
+          required
         ></input>
         <label htmlFor="subject">Subject:</label>
         <input
           type="text"
           className={styles.subject_input}
           value={subject}
-          onChange={(e) => setSubject(e.target.value)}
+          ref={subjectRef}
+          required
         ></input>
         <textarea
           className={styles.messageArea}
           placeholder="Text"
           value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          ref={messageRef}
+          required
         ></textarea>
-        <button className={styles.formButtons}> Send</button>
+        <button className={styles.formButtons} type="submit">
+            Send
+        </button>
         <button className={styles.formButtons}>Cancel</button>
       </form>
     </div>
