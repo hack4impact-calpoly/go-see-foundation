4ad22bceb638723799b2d941ac91755e6e45d3a3
tammy-si <sitammy6@gmail.com>
2024-04-21 13:53:53 -0700

    Changed the styles for email page

--- src/app/admin/email/page.tsx
+++ src/app/admin/email/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useRef } from "react";
 import styles from "./emailPage.module.css";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
 export default function AdminPageEmail() {
   const [group, setGroup] = useState("Test1");
@@ -15,46 +15,34 @@ export default function AdminPageEmail() {
   const subjectRef = useRef<HTMLInputElement>(null);
   const messageRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleSend = async (event: React.FormEvent<HTMLFormElement>) =>{
+  const handleSend = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
-        const emailParams = {
-            to_name: toRef.current?.value, 
-            subject: subjectRef.current?.value,
-            message: messageRef.current?.value,
-        };
-    
-        await emailjs.send('service_9qu0p52', 'template_99zsbe8', emailParams, "Zw5OFi0OEo2qOzWRb");
-        alert("Email sent successfully.");
+      const emailParams = {
+        to_name: toRef.current?.value,
+        subject: subjectRef.current?.value,
+        message: messageRef.current?.value,
+      };
 
-        setTo("");
-        setSubject("");
-        setMessage("");
+      await emailjs.send(
+        "service_9qu0p52",
+        "template_99zsbe8",
+        emailParams,
+        "Zw5OFi0OEo2qOzWRb"
+      );
+      alert("Email sent successfully.");
+
+      setTo("");
+      setSubject("");
+      setMessage("");
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-}
+  };
 
   return (
     <div className={styles.emailArea}>
-      <div className={styles.scrollSide}>
-        <div className={styles.scrollDropdown}>
-          <select onChange={(e) => setScrollDrop(e.target.value)}>
-            <option>Inbox</option>
-            <option>Test</option>
-          </select>
-        </div>
-        <div className={styles.emailList}>
-          <EmailPreview />
-          <EmailPreview />
-          <EmailPreview />
-          <EmailPreview />
-          <EmailPreview />
-          <EmailPreview />
-          <EmailPreview />
-        </div>
-      </div>
       <form className={styles.emailForm} onSubmit={handleSend}>
         <label htmlFor="group">Select Group:</label>
         <select
@@ -95,7 +83,7 @@ export default function AdminPageEmail() {
           required
         ></textarea>
         <button className={styles.formButtons} type="submit">
-            Send
+          Send
         </button>
         <button className={styles.formButtons}>Cancel</button>
       </form>
