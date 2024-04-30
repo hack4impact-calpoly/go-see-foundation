b785f784a4b01ef7dfb5f734b8376a1bdcea0747
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-04-30 16:05:24 -0700

    add: resource schema

---
+++ src/database/resourceSchema.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
+
+export type IResource = {
+    picture: string;
+    alt: string;
+    title: string;
+    url: string;
+
+};
+
+const resourceSchema = new Schema<IResource>({
+    picture: { type: String, required: true },
+    alt: { type: String, required: true },
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+});
+
+const Resource =
+  mongoose.models["resources"] || mongoose.model("resources", resourceSchema);
+
+export default Resource;
