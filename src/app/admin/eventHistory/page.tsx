80ee29086e95b539c6135045818154a15b26fbc7
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2024-05-15 18:15:59 -0700

    added skeleton for fetched events from db

---
+++ src/app/admin/eventHistory/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./eventHistory.module.css";
+
+export default function EventHistory() {
+  let [events, setEvents] = useState<any[]>([]);
+
+  const fetchAllEvents = async () => {
+    try {
+      const res = await fetch("/api/events/", {
+        method: "GET",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch Events");
+      }
+
+      const res_j = await res.json();
+      return res_j;
+    } catch (err: unknown) {
+      console.error(`Error: ${err}`);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        let data = await fetchAllEvents();
+
+        setEvents(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchEventData();
+  }, []);
+
+  return (
+    <div className={styles.eventHistory}>
+      <div className={styles.formBody}>
+        <h1 className={styles.eventTitle}>Events</h1>
+
+        <div className={styles.eventsBody}>
+          {events &&
+            events.map((event, index) => (
+              <div key={index} className={styles.eventItem}>
+                <h2>{event.name}</h2> <p>edit event</p> <p>view Signees</p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
