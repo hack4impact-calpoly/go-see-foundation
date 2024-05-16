d0e0aa2ce63dcfdfa2f1650a1d54e9d5eb1c74a3
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2024-05-16 01:55:46 -0700

    added basic event attendance design

--- src/app/admin/eventHistory/[eventName]/page.tsx
+++ src/app/admin/eventHistory/[eventName]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
+import emailIcon from "../../../images/emailIcon.png";
+import expandDown from "../../../images/Expand_down-2.png";
 
+import Image from "next/image";
 import styles from "./eventName.module.css";
 import { useEffect, useState } from "react";
 
@@ -39,14 +42,41 @@ export default function eventName(context: any) {
 
   return (
     <div className={styles.eventHistory}>
-      <div className={styles.eventTitle}>{eventName}</div>
+      <div className={styles.bodyCard}>
+        <div className={styles.eventTitle}>{eventName}</div>
 
-      {members &&
-        members.map((mem, index) => (
-          <div key={index} className={styles.body}>
-            <h2 className={styles.memberCard}>{mem.firstName + mem.lastName}</h2>
+        {members && members.length > 0 ? (
+          members.map((mem, index) => (
+            <div key={index} className={styles.body}>
+              <p className={styles.textCard}>
+                {mem.firstName + " " + mem.lastName + " (" + mem.userType + ")"}
+              </p>
+              <p className={styles.textCard}>{mem.phoneNum}</p>
+
+              <div className={styles.dropdownImg}>
+                <Image
+                  src={emailIcon}
+                  alt="background logo for the Go See Foundation"
+                  height="30"
+                  width="30"
+                  priority={true}
+                />
+                <Image
+                  src={expandDown}
+                  alt="background logo for the Go See Foundation"
+                  height="30"
+                  width="30"
+                  priority={true}
+                />
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className={styles.eventHistory}>
+            <div className={styles.bodyCard}>No Members Found.</div>
           </div>
-        ))}
+        )}
+      </div>
     </div>
   );
 }
