294bc9371c240d006b26e3ee49eb4df694735f02
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2025-02-07 00:15:05 -0800

    add: update blogs route + frontend/backend integration

--- src/app/api/blog/[blogID]/route.ts
+++ src/app/api/blog/[blogID]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@database/db";
-import BlogSchema from "@database/blogSchema";
+import BlogSchema, { IBlog } from "@database/blogSchema";
 
 type IParams = {
   params: {
@@ -21,6 +21,39 @@ export async function DELETE(req: NextRequest, { params }: IParams) {
   } catch (err) {
     return NextResponse.json(
       { message: `Blog not found. Error: ${err}` },
+      { status: 404 }
+    );
+  }
+}
+
+
+export async function PUT(req: NextRequest, { params }: IParams) {
+  await connectDB();
+  const { blogID } = params;
+
+  try {
+    const updatedData: Partial<IBlog> = await req.json();
+
+    if (!Object.keys(updatedData).length) {
+      return NextResponse.json(
+        { message: "Failed: No fields provided to update" },
+        { status: 400 }
+      );
+    }
+
+    const updatedBlog = await BlogSchema.findOneAndUpdate(
+      { blogID },
+      updatedData,
+      { new: true, runValidators: true } 
+    ).orFail();
+
+    return NextResponse.json(
+      { message: "Success: Blog updated", updatedBlog },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { message: `Failed: Blog not found or could not be updated. Error: ${err}` },
       { status: 404 }
     );
   }
